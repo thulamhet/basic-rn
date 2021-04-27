54cb7e5e40516ebd1ff46c42c945d9abc93b1b23
thulamhet <61628978+thulamhet@users.noreply.github.com>
2021-04-27 16:41:11 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import FlatListTest from './Components/FlatListdemo';
+
+
+
+const App = () => {
+  return (
+    <FlatListTest/>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
