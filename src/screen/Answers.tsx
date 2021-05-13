d45065d5f72ba463bcee2ca21a090a8f67de258f
cuong.dang1 <cuong.dang1@onemount.com>
2021-05-13 19:34:07 +0700

    lesson 8 context

--- src/screen/Answers.tsx
+++ src/screen/Answers.tsx
@@ -1,21 +1,28 @@
-import React, {useContext} from "react";
+import React from "react";
 import {Text, View} from "react-native";
 import {AnswerContext} from "../../App";
 
 const Answers: React.FC = () => {
-    const context = useContext(AnswerContext);
-    const {answers} = context;
-    const {survey1Answer, survey2Answer} = answers;
     return (
-        <View>
-            <Text>
-                Đáp án đã chọn:
-            </Text>
-            <Text>survey1 : </Text>
-            <Text>{survey1Answer.join(',')}</Text>
-            <Text>survey2 : </Text>
-            <Text>{survey2Answer.join(',')}</Text>
-        </View>
+        <AnswerContext.Consumer>
+            {
+                value => {
+                    const {answers} = value;
+                    const {survey1Answer, survey2Answer} = answers;
+                    return <View>
+                        <Text>
+                            Đáp án đã chọn:
+                        </Text>
+                        <Text>survey1 : </Text>
+                        <Text>{survey1Answer.join(',')}</Text>
+                        <Text>survey2 : </Text>
+                        <Text>{survey2Answer.join(',')}</Text>
+                    </View>
+                }
+            }
+
+        </AnswerContext.Consumer>
+
     )
 }
 
